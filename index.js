d4f4baff8dfc6da682f4c18eafd441c7b54b0ae0
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-05-08 12:43:07 +0200

    Add `bare-debug-log`

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ exports.formatWithOptions = exports.format.formatWithOptions
 
 exports.inspect = require('bare-inspect')
 
+exports.debuglog = require('bare-debug-log')
+
 exports.inherits = function inherits (ctor, superCtor) {
   Object.setPrototypeOf(ctor.prototype, superCtor.prototype)
 }
