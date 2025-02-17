94c9c718ecc44766e0443846f70c1f2e1488f919
supersuryaansh <prod.genx@gmail.com>
2025-02-17 15:42:02 +0530

    Return function wrapper in deprecate

--- index.js
+++ index.js
@@ -11,5 +11,13 @@ exports.inherits = function inherits (ctor, superCtor) {
 }
 
 exports.deprecate = function deprecate (fn, msg, code) {
-  console.warn(`[${code}]`, 'DeprecationWarning:', msg)
+  let warned = false // Track if the warning has already been logged
+
+  return function (...args) {
+    if (!warned) {
+      console.warn(`[${code}]`, 'DeprecationWarning:', msg)
+      warned = true
+    }
+    return fn.apply(this, args)
+  }
 }
