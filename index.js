304c55e584c00dba11bb6d40607e794f134acae8
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-03-11 13:51:35 +0100

    Support constructors and only log `code` if provided

--- index.js
+++ index.js
@@ -11,13 +11,21 @@ exports.inherits = function inherits(ctor, superCtor) {
 }
 
 exports.deprecate = function deprecate(fn, msg, code) {
-  let warned = false // Track if the warning has already been logged
+  let warned = false
 
   return function (...args) {
     if (!warned) {
-      console.warn(`[${code}]`, 'DeprecationWarning:', msg)
       warned = true
+
+      if (code) {
+        console.warn(`[${code}]`, 'DeprecationWarning:', msg)
+      } else {
+        console.warn('DeprecationWarning:', msg)
+      }
     }
-    return fn.apply(this, args)
+
+    if (new.target) return Reflect.construct(fn, args, new.target)
+
+    return Reflect.apply(fn, this, args)
   }
 }
